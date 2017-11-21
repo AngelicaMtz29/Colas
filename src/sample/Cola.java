c69804ec54e6e98fcbf2d8f849db47eef0389229
AbelAragon10UTIM <cgithubabel@gmail.com>
2017-11-21 15:22:57 -0600

    Mi aportación

--- src/sample/Cola.java
+++ src/sample/Cola.java
@@ -1,4 +1,4 @@
-package sample;
+﻿package sample;
 
 import javafx.scene.control.Label;
 
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+﻿package sample;
 
 import com.sun.javafx.scene.layout.region.Margins;
 import javafx.event.ActionEvent;
@@ -26,11 +26,47 @@ public class Controller implements Initializable{
     @FXML
     HBox ColaHbx;
 
+   
+    Cola cola = new Cola();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+      
+            this.InsertarBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cola.insertar(Integer.parseInt(valorTxt.getText()));
+                cola.mostrar();
+                Separator separator = new Separator();
+                HBox hBox = new HBox();
+                Label labell = new Label (valorTxt.getText());
+                Separator separador = new Separator();
 
+                hBox.getChildren().add(labell);
+                hBox.getChildren().add(separador);
+                colasHbx.getChildren().add(hBox);
+
+                separador.setOpacity(0);
+                separador.setMaxWidth(60);
+                separator.setOpacity(0);
+                separador.setMaxWidth(30);
+
+                Color seletedColor = colorPicker.getValue();
+                hBox.setBackground(new Background(new BackgroundFill(Paint.valueOf(seletedColor.toString()), CornerRadii.EMPTY, Insets.EMPTY)));
+
+            }
+        });
+
+        this.BuscarBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cola.buscar(Integer.parseInt(valorTxt.getText()));
+            }
+        });
+
+
+       
 
     }
 
