50784f33c901c02cc00ad6d851054d5c50ea56bc
Angélica <angelica.martinez16@utim.edu.mx>
2017-11-23 05:27:01 -0600

    Listo para fusión

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -22,7 +22,7 @@ public class Controller implements Initializable{
     @FXML
     Button InsertarBtn, BuscarBtn, EliminarBtn, VaciarBtn,TamañoBtn ;
     @FXML
-    TextField ValorTxt;
+    TextField valorTxt;
     @FXML
     HBox ColaHbx;
 
@@ -31,39 +31,6 @@ public class Controller implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-      
-            this.InsertarBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                cola.insertar(Integer.parseInt(valorTxt.getText()));
-                cola.mostrar();
-                Separator separator = new Separator();
-                HBox hBox = new HBox();
-                Label labell = new Label (valorTxt.getText());
-                Separator separador = new Separator();
-
-                hBox.getChildren().add(labell);
-                hBox.getChildren().add(separador);
-                colasHbx.getChildren().add(hBox);
-
-                separador.setOpacity(0);
-                separador.setMaxWidth(60);
-                separator.setOpacity(0);
-                separador.setMaxWidth(30);
-
-                Color seletedColor = colorPicker.getValue();
-                hBox.setBackground(new Background(new BackgroundFill(Paint.valueOf(seletedColor.toString()), CornerRadii.EMPTY, Insets.EMPTY)));
-
-            }
-        });
-
-        this.BuscarBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                cola.buscar(Integer.parseInt(valorTxt.getText()));
-            }
-        });
 
 
        
